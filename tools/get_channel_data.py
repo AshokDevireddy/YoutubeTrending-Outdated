e6b50b0d91b7c1e405a26f766e86a7ea440b142b
Armeet Singh Jatyani <xarmeetx@gmail.com>
2019-11-10 23:17:31 -0800

    finished search by video name request

--- tools/get_channel_data.py
+++ tools/get_channel_data.py
@@ -2,8 +2,15 @@ import os
 import json
 import requests
 
-API_TOKEN = os.environ['DEVELOPER_KEY'];
-API_URL_BASE = 'http://api.wolframalpha.com/v1/simple?appid=DEMO'
+API_TOKEN = 'AIzaSyDfzZw-wSX8NfI2vamY38igjIkIMimv_eA'
+API_URL_BASE = 'https://www.googleapis.com/youtube/v3'
 
-response = requests.get(API_URL_BASE + "&i=What+airplanes+are+flying+overhead%3F")
-print(response)
+def search_vids_keyword(search_keyword):
+#    response = requests.get("%s/search?part=snippet&maxResults=50&q=%s&%s" (API_URL_BASE, search_keyword, API_TOKEN)
+    response = requests.get("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=a&key=AIzaSyDfzZw-wSX8NfI2vamY38igjIkIMimv_eA")
+    print(json.loads(response.content))
+    f = open("tools/response.json","w+")
+    f.write(json.dumps(json.loads(response.content), indent = 4, separators = (", ", "= ")))
+    f.close();
+
+search_vids_keyword('a');
